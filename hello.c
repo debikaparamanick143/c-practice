1803ecae6ff70ad15ba135bfa9529d364e432b9c
Debika <debikaparamanick560@gmail.com>
2026-07-25 01:47:42 +0800

    Associativity

--- hello.c
+++ hello.c
@@ -1,14 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 int main (){
-    int b,c;
-    b=c=1;
-    int a = b+c;
-    int power =pow (b,c);
-    printf ("%d", power);
-    
-
-
-
-    return 0;
+   int a= 4+9*10;
+   printf ("%d \n", a);
+;    return 0;
 }
