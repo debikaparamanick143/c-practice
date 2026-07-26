6d81cbd20102ee6fdbc6841a16ec15cb7b667942
Debika <debikaparamanick560@gmail.com>
2026-07-26 22:09:36 +0800

    Logic gate (OR)

--- hello.c
+++ hello.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 int main (){
-   int x;
-   printf ("enter a number");
-   scanf ("%d", & x);
-   printf ("%d", x%4==0);
-    
+  int issunday=0;
+  int issnowing=1;
+  printf ("%d \n", issunday || issnowing);
 
    return 0;
 }
