68fd3d29625221ed8e20b140205bd6baf98bbf68
Debika <debikaparamanick560@gmail.com>
2026-07-27 01:56:22 +0800

    (switch case) day of week program in hello.c

--- hello.c
+++ hello.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
-#include<math.h>
+
 int main (){
-  int age;
-  printf ("enter age");
-  scanf ("%d", &age);
-
-  if (age>18){
-   printf ("adult \n");
-   printf ("they can drive");
-   printf ("they can vote \n");
-   printf ("they can do everything \n");
-
-  }
-  else {
-   printf ("not adult");
-  }
+   int day;// 1-mon; 2-tues; 3-wed
+   printf ("enter day (1-7)");
+   scanf ("%d", &day);
+   switch (day) {
+      case 1: printf ("monday \n");
+            break;
+      case 2:printf ("tuesday \n");
+            break;
+      case 3:printf ("wednesday \n");
+            break;
+      case 4:printf ("thrusday \n");
+            break;
+      case 5:printf ("friday \n");
+           break;
+      case 6:printf ("saturday \n");
+            break;
+   }
    return 0;
-}
+   }
