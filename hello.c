6f9faa2769c4362ed5f1c8302560a875a9ede076
Debika <debikaparamanick560@gmail.com>
2026-07-26 22:45:47 +0800

    Age check program in hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 #include<math.h>
 int main (){
-  int isMonday=1;
-  int isRaining=1;
-  printf ("%d \n",isMonday && isRaining);
+  int age;
+  printf ("enter age");
+  scanf ("%d", &age);
 
+  if (age>18){
+   printf ("adult \n");
+   printf ("they can drive");
+   printf ("they can vote \n");
+   printf ("they can do everything \n");
+
+  }
+  else {
+   printf ("not adult");
+  }
    return 0;
 }
