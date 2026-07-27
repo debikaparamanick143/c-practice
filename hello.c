8f0e10633281d2a2ad8613a23b7ce67acb76959d
Debika <debikaparamanick560@gmail.com>
2026-07-27 12:31:46 +0800

    Pass or Fail program

--- hello.c
+++ hello.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 
 int main (){
-   int day;// 1-mon; 2-tues; 3-wed
-   printf ("enter day (1-7)");
-   scanf ("%d", &day);
-   switch (day) {
-      case 1: printf ("monday \n");
-            break;
-      case 2:printf ("tuesday \n");
-            break;
-      case 3:printf ("wednesday \n");
-            break;
-      case 4:printf ("thrusday \n");
-            break;
-      case 5:printf ("friday \n");
-           break;
-      case 6:printf ("saturday \n");
-            break;
+   int marks;
+   printf ("enter number (0-100)");
+   scanf ("%d", &marks);
+
+   if (marks <=30){
+      printf ("FAIL \n");
+   }else{
+      printf ("PASS \n");
    }
+
    return 0;
    }
