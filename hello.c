7fc318317d9a20c6a032f99d44a81643a01f8fd7
Debika <debikaparamanick560@gmail.com>
2026-07-29 21:19:40 +0800

    Multiplication table program

--- hello.c
+++ hello.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-
 int main (){
- int i=1;
- do {
-  printf ("%d \n", i);
-  i++;
-}while (i<=29);
+  int n;
+  printf ("enter number :");
+  scanf ("%d \n", & n);
 
-  return 0;
+  for (int i=1; i<=10; i++){
+    printf ("%d \n", n*i);
+  }
 }
