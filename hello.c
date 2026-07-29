06607f7b1f478636f53452536ad1d75ff54080e7
Debika <debikaparamanick560@gmail.com>
2026-07-29 11:38:24 +0800

    Do while Loop

--- hello.c
+++ hello.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
 int main (){
-  int i=1;
-  while (i<=5){
-    printf ("helo world \n");
-    i++;
-  }
-  
+ int i=1;
+ do {
+  printf ("%d \n", i);
+  i++;
+}while (i<=29);
 
-   return 0;
-} 
+  return 0;
+}
