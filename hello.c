2c31c42f51828a890685becde706723cdf5ae2ed
Debika <debikaparamanick560@gmail.com>
2026-07-30 23:59:58 +0800

    (1-10) except for 6

--- hello.c
+++ hello.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 int main (){
-int n;
-printf ("enter number:");
-scanf ("%d",& n);
-
-int fact =1;
-for (int i=1; i<=n; i++){
-   fact =fact*i;
+for (int i=1; i<=10; i++){
+   if (i==6){ // skip
+      continue;
+   }
+   printf ("%d \n ", i);
 }
-printf ("final factorial is %d", fact);
    return 0;
 }
